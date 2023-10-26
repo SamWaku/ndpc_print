4f8aa3dbd535c909869f0b284ea5fb1d3fea503e
cmdpappi <cmdpappi@terminalpappi.myguest.virtualbox.org>
2023-10-26 11:00:03 +0200

    doctypes

---
+++ ndpc/ndpc/doctype/memo/api.py
@@ -0,0 +1,31 @@
+import frappe
+
+@frappe.whitelist()
+def user_role(email):
+     # Get the current user
+    user = frappe.session.user
+
+    # Fetch the user's roles
+    # user_roles = frappe.get_roles(user)
+    user_roles = frappe.db.sql(f"""SELECT role_profile_name FROM `tabUser` WHERE email='{email}';""")
+
+    # Convert the list of roles to a string
+    user_roles_string = str(user_roles)
+    user_roles_string_converted = user_roles_string[3:-5]
+
+    # Log the user's roles
+    # frappe.msgprint(f"User Roles for {user}: {user_roles_string}")
+    return user_roles_string_converted
+
+    
+    # # Get the current user
+    # user = frappe.session.user
+
+    # # Fetch the user's roles
+    # user_roles = frappe.get_roles(user)
+
+    # # Log the user's roles
+    # # frappe.msgprint(f"User Roles for {user}: {user_roles}")
+    # return user_roles
+
+    # # return frappe.db.sql(f"""SELECT role_profile_name FROM `tabUser` WHERE email='{email}';""")
---
+++ ndpc/ndpc/doctype/memo/test_memo.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2023, sam | iyke and Contributors
+# See license.txt
+
+# import frappe
+from frappe.tests.utils import FrappeTestCase
+
+
+class TestMemo(FrappeTestCase):
+	pass
